49191e587a5b264e09a4c52eec56c64b344a7412
PavonEvelin <evelinluz17@gmail.com>
2020-05-19 14:11:57 -0300

    Primer commit en VS

---
+++ TP6-TallerdeLenguajes/TP6-TallerdeLenguajes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP6_TallerdeLenguajes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!");
+        }
+    }
+}
