b62b53ea45506d17b625c191ff840e3ccfaf80c1
PavonEvelin <evelinluz17@gmail.com>
2020-05-24 17:07:38 -0300

    Agregado punto 1

---
+++ TP6-TDL/TP6_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TP6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, resto;
+            Console.WriteLine("Ingrese un numero\n");
+            num = int.Parse(Console.ReadLine());
+            //Console.WriteLine("numero: " + num);
+            if(num <= 0)
+            {
+                Console.WriteLine("No se puede invertir el número\n");
+            }
+            else
+            {
+                while (num > 0)
+                {
+                    resto = num % 10;
+                    Console.WriteLine("" + resto);
+                    num = num / 10;
+                }
+            }
+            
+        }
+    }
+}
